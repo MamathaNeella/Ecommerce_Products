add5858fc78a2d319d9ccc6c2fc8a4ba82c6cdb1
MamathaNeella <157876261+MamathaNeella@users.noreply.github.com>
2024-02-12 18:48:25 +0530

    Added products table with data

--- Proj1_Products.sql
+++ Proj1_Products.sql
@@ -3,4 +3,11 @@
   Prod_Name varchar(30) unique not null,
   Quantity int not null default 1);
   
+  insert into products(prod_name,quantity)
+  values('Samsung A71', 89),('Iphone 14plus', 56),
+  ('Oppo 3series', 19),('Realme', 32),
+  ('Vivo', 65),('Oneplus Note', 38),
+  ('Xiomi', 17),('Redme', 43),
+  ('Moto 5g', 52),('Google', 28),
+  ('Nokia', 64),('Jio', 29);
   
