e5a95bb8d45bd32d8b4e8a54ad17652ca4b88c66
MamathaNeella <157876261+MamathaNeella@users.noreply.github.com>
2024-02-13 15:36:41 +0530

    Added a trigger to update products quantity

--- proj1_joins.sql
+++ proj1_joins.sql
@@ -4,3 +4,10 @@
   on C.cust_id=O.cust_id
   inner join Products as P
   on O.prod_id=P.prod_id;
+  
+CREATE TRIGGER update_product_quantityCust_id
+AFTER INSERT ON orders
+FOR EACH ROW
+UPDATE products
+SET quantity = quantity - NEW.quantity
+WHERE prod_id = NEW.prod_id;
