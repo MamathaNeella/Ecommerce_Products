12faffa87178d240e8a3052c47b871033f42a55b
MamathaNeella <157876261+MamathaNeella@users.noreply.github.com>
2024-02-14 17:27:38 +0530

    Added case statement to orders table

--- proj1_joins.sql
+++ proj1_joins.sql
@@ -11,3 +11,13 @@ FOR EACH ROW
 UPDATE products
 SET quantity = quantity - NEW.quantity
 WHERE prod_id = NEW.prod_id;
+
+  select case 
+  when quantity=1 then "one item sold"
+  when quantity=2 then "two items sold"
+  when quantity=3 then "three items sold"
+  when quantity>=4 then "more than three items sold"
+  when quantity=0 then "no items sold"
+  else "no order"
+  end as order_quantity
+  from orders;
